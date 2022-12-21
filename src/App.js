4a7c127896b2899e11f9411d695824388db462b1
BHASKAR RANJAN BORA <30873007+bhskr44@users.noreply.github.com>
2022-12-22 02:45:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import MissionPage from './components/mission/MissionPage';
+import ProfilePage from './components/profile/ProfilePage';
+import RocketPage from './components/rocket/RocketPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>React App</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<RocketPage />} />
+        <Route path="/missions" element={<MissionPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
