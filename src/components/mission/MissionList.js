c7eaaac46977eafd23c1776b48c5ae60d9fed054
BHASKAR RANJAN BORA <30873007+bhskr44@users.noreply.github.com>
2022-12-23 19:57:15 +0530

    Add Mission Table Css

--- src/components/mission/MissionList.js
+++ src/components/mission/MissionList.js
@@ -3,21 +3,23 @@ import Mission from './Mission';
 
 const MissionList = ({ missions }) => (
   <div className="rocket-list">
-    <table>
-      <tr>
-        <th>
-          <h3>Mission</h3>
-        </th>
-        <th>
-          <h3>Description</h3>
-        </th>
-        <th>
-          <h3>Status</h3>
-        </th>
-        <th>
-          <h3>&nbsp;</h3>
-        </th>
-      </tr>
+    <table className="mission-table">
+      <thead>
+        <tr>
+          <th className="td-name">
+            <h3>Mission</h3>
+          </th>
+          <th className="td-description">
+            <h3>Description</h3>
+          </th>
+          <th className="td-status">
+            <h3>Status</h3>
+          </th>
+          <th className="td-button">
+            <h3>&nbsp;</h3>
+          </th>
+        </tr>
+      </thead>
       {missions.map((mission) => (
         <Mission mission={mission} key={mission.mission_id} />
       ))}
--- src/components/profile/ProfilePage.js
+++ src/components/profile/ProfilePage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import OptMission from './OptMission';
 import OptRocket from './OptRocket';
 
 const ProfilePage = () => (
   <div className="profile-div">
+    <OptMission />
     <OptRocket />
   </div>
 );
